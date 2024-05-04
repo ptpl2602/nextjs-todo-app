fb1e58b498bfe67f76745df9fb393f9dcaea07af
ptpl2602 <phuonglinhpham2602@gmail.com>
2024-05-04 11:16:15 +0700

    fix some bug feature-2

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -30,17 +30,19 @@ export default function ModalAddTask({modalOpen, setModalOpen, taskValue, setTas
                 id: uuid,
                 text: taskValue
             })
+            setTaskValue('');
         }
 
-        setTaskValue('');
         setModalOpen(true);
         console.log(taskValue);
         router.refresh();
     }
 
     function handleCloseModal() {
-        setTaskValue('')
         setModalOpen(false)
+        if(!isEditing) {
+            setTaskValue('')
+        }
     }
 
     return (
@@ -48,12 +50,14 @@ export default function ModalAddTask({modalOpen, setModalOpen, taskValue, setTas
         id="authentication-modal"
         tabIndex={-1}
         aria-hidden="true"
-        className={`${ modalOpen ? "flex" : "hidden" } fixed inset-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+        className={`${
+          modalOpen ? "flex" : "hidden"
+        } fixed inset-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="relative p-4 w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <div className="flex items-center justify-between p-4 md:p-4 border-b rounded-t dark:border-gray-600">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                 {isEditing ? "Update task" : "Add new task"}
               </h3>
@@ -82,21 +86,23 @@ export default function ModalAddTask({modalOpen, setModalOpen, taskValue, setTas
               </button>
             </div>
 
-            <div className="p-4 md:p-5">
-              <form className="space-y-4" onSubmit={handleSubmitTask}>
+            <div className="p-3 md:p-5">
+              <form className="space-x-3 flex items-center justify-between" onSubmit={handleSubmitTask}>
                 <input
-                    value={taskValue}
-                    onChange={e => setTaskValue(e.target.value)}
+                  value={taskValue}
+                  onChange={(e) => setTaskValue(e.target.value)}
                   type="text"
                   className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                   placeholder="Ex: to cook for lunch"
                   required
                 />
                 <button
-                   type="submit"
-                   className="w-2/6 text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30"
+                  type="submit"
+                  className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30"
                 >
-                  <span className="font-semibold uppercase">{isEditing ? 'Update' : 'Submit'}</span>
+                  <span className="font-semibold uppercase">
+                    {isEditing ? "Update" : "Submit"}
+                  </span>
                 </button>
               </form>
             </div>
